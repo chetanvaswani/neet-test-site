904c24eb2e379aba865123aae588e21c980626c2
chetanvaswani <chetan.vaswani.cv@gmail.com>
2025-03-20 20:18:03 +0530

    questions: atomic structure

---
+++ src/questions/atomic-structure.js
@@ -0,0 +1,170 @@
+const QUESTIONS = [
+    {
+      queNum: 1,
+      question: "What is Bohr’s radius of the 4th orbit of He+ in Å?",
+      options: [
+        "5.23 Å",
+        "3.23 Å",
+        "6.23 Å",
+        "4.23 Å"
+      ],
+      answer: "4.23 Å",
+    },
+    {
+      queNum: 2,
+      question: "The energy of radiation emitted when an electron falls from n = 3 to n = 2 level in a hydrogen atom will be: (RH = 2.18 x 10⁻¹⁸ J atom⁻¹)",
+      options: [
+        "0.3 x 10⁻¹⁸ J atom⁻¹",
+        "3 x 10⁻¹⁸ J atom⁻¹",
+        "0.03 x 10⁻¹⁸ J atom⁻¹",
+        "3 x 10⁻¹⁷ J atom⁻¹"
+      ],
+      answer: "0.3 x 10⁻¹⁸ J atom⁻¹",
+    },
+    {
+      queNum: 3,
+      question: "How fast does light travel in vacuum as compared to the velocity of an electron in Bohr’s first orbit of a hydrogen atom?",
+      options: [
+        "13.7 times",
+        "67 times",
+        "137 times",
+        "97 times"
+      ],
+      answer: "137 times",
+    },
+    {
+      queNum: 4,
+      question: "The first emission lines of the hydrogen atomic spectrum in the Balmer series appear at:",
+      options: [
+        "5R/36 cm⁻¹",
+        "3R/4 cm⁻¹",
+        "7R/144 cm⁻¹",
+        "9R/400 cm⁻¹"
+      ],
+      answer: "5R/36 cm⁻¹",
+    },
+    {
+      queNum: 5,
+      question: "Quantum numbers are different quantities used to describe the electrons present in an atom. Identify the quantum number that determines the orientation of the orbital in space.",
+      options: [
+        "Azimuthal quantum number",
+        "Spin quantum number",
+        "Magnetic quantum number",
+        "Principal quantum number"
+      ],
+      answer: "Magnetic quantum number",
+    },
+    {
+      queNum: 6,
+      question: "Which one of the following about an electron occupying the 1s orbital in a hydrogen atom is incorrect? (The Bohr radius is represented by a₀)",
+      options: [
+        "The total energy of the electron is maximum when it is at a distance a₀ from the nucleus.",
+        "The probability density of finding the electron is maximum at the nucleus.",
+        "The electron can be found at a distance of 2a₀ from the nucleus.",
+        "The magnitude of the potential energy is double that of its kinetic energy on average."
+      ],
+      answer: "The total energy of the electron is maximum when it is at a distance a₀ from the nucleus.",
+    },
+    {
+      queNum: 7,
+      question: "What is the de Broglie wavelength of the electron accelerated at 400 V, approximately?",
+      options: [
+        "1.00 nm",
+        "0.12 nm",
+        "0.08 nm",
+        "0.06 nm"
+      ],
+      answer: "0.06 nm",
+    },
+    {
+      queNum: 8,
+      question: "The uncertainty in the position of an electron is equal to its de Broglie wavelength. The minimum percentage error in its measurement of velocity under this circumstance will be approximate:",
+      options: [
+        "4",
+        "8",
+        "2",
+        "18"
+      ],
+      answer: "8",
+    },
+    {
+      queNum: 9,
+      question: "From the following sets of quantum numbers, which state is possible?\n(i) n = 0, l = 0, mₗ = 1, mₛ = +1/2\n(ii) n = 2, l = 1, mₗ = 0, mₛ = –1/2\n(iii) n = 2, l = 0, mₗ = 3, mₛ = +1/2\n(iv) n = 3, l = 1, mₗ = 0, mₛ = –1/2",
+      options: [
+        "(i), (ii), (iii)",
+        "(ii) and (iv) are possible",
+        "All are not possible",
+        "All are possible"
+      ],
+      answer: "(ii) and (iv) are possible",
+    },
+    {
+      queNum: 10,
+      question: "When the 3d orbital is complete, the new electron will enter the:",
+      options: [
+        "4p orbital",
+        "4f orbital",
+        "4s orbital",
+        "4d orbital"
+      ],
+      answer: "4p orbital",
+    },
+    {
+      queNum: 11,
+      question: "What should be the maximum number of lines obtained in the spectrum, if the total number of energy levels is five?",
+      options: [
+        "10",
+        "6",
+        "4",
+        "2"
+      ],
+      answer: "10",
+    },
+    {
+      queNum: 12,
+      question: "We can distinguish two electrons occupying the same orbital by:",
+      options: [
+        "Principal quantum number",
+        "Azimuthal quantum number",
+        "Magnetic quantum number",
+        "Spin quantum number"
+      ],
+      answer: "Spin quantum number",
+    },
+    {
+      queNum: 13,
+      question: "In which quantum level does the electron jump in He+ ion to ground state if it is given an energy corresponding to 99% of the ionisation potential of He+ ion?",
+      options: [
+        "4",
+        "6",
+        "8",
+        "10"
+      ],
+      answer: "10",
+    },
+    {
+      queNum: 14,
+      question: "The value of quantum numbers (n, l, m and s, respectively) for the valence electron of Rb (Rubidium) is:",
+      options: [
+        "5, 0, 0, +1/2",
+        "5, 1, 0, +1/2",
+        "5, 1, 1, +1/2",
+        "None of these"
+      ],
+      answer: "5, 0, 0, +1/2",
+    },
+    {
+      queNum: 15,
+      question: "Which statement does not form part of Bohr’s model of the hydrogen atom?",
+      options: [
+        "The energy of the orbit is quantised.",
+        "Electrons in orbit nearest to the nucleus have the lowest energy.",
+        "Electrons revolve in different orbits around the nucleus.",
+        "The position and velocity of electrons in orbit cannot be determined simultaneously."
+      ],
+      answer: "The position and velocity of electrons in orbit cannot be determined simultaneously.",
+    }
+  ];
+  
+  export default QUESTIONS;
+  
