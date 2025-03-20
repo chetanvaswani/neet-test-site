224d2e6f4b9f7b56bf0b9ebbe4c6f20fba55fdaf
chetanvaswani <chetan.vaswani.cv@gmail.com>
2025-03-21 02:29:18 +0530

    question chemical bonding 2nd test

---
+++ src/questions/chemical-bonding-2.js
@@ -0,0 +1,117 @@
+const QUESTIONS = [
+    {
+      queNum: 1,
+      question: "Find correct set of formal charge on marked oxygen respectively.",
+      options: [
+        "(1) +1, +1, +1",
+        "(2) +1, –1, 0",
+        "(3) +1, 0, –1",
+        "(4) 0, +1, –1"
+      ],
+      answer: "(3) +1, 0, –1",
+    img: "chemical-bonding-1.png"
+    },
+    {
+      queNum: 2,
+      question: "Sulphur dichloride–",
+      options: [
+        "(1) Is a hyper valent molecule",
+        "(2) Is a hypo valent molecule",
+        "(3) Obeys octet rule",
+        "(4) None of the above"
+      ],
+      answer: "(3) Obeys octet rule",
+    },
+    {
+      queNum: 3,
+      question: "Ionic bond forms more easily between elements with comparatively.",
+      options: [
+        "(1) Low I.E. and high negative value of EGE",
+        "(2) High I.E. and high negative value of EGE",
+        "(3) High I.E. and low negative value of EGE",
+        "(4) Low I.E. and low negative value of EGE"
+      ],
+      answer: "(1) Low I.E. and high negative value of EGE"
+    },
+    {
+      queNum: 4,
+      question: "Compare bond length in following –",
+      options: [
+        "(1) C – N < C = N < C ≡ N",
+        "(2) C – N < C = N > C ≡ N",
+        "(3) C – N > C = N > C ≡ N",
+        "(4) C ≡ N < C – N < C = N"
+      ],
+      answer: "(3) C – N > C = N > C ≡ N"
+    },
+    {
+      queNum: 5,
+      question: "Assertion – HCl being covalent molecule contain certain percentage of ionic character.\nReason – In reality no bond or a compound is either completely covalent or ionic.",
+      options: [
+        "(1) Both Assertion and Reason are true and the Reason is a correct explanation of the Assertion.",
+        "(2) Both Assertion and Reason are true but Reason is not a correct explanation of the Assertion.",
+        "(3) Assertion is true but Reason is false.",
+        "(4) Both Assertion and Reason are false."
+      ],
+      answer: "(1) Both Assertion and Reason are true and the Reason is a correct explanation of the Assertion."
+    },
+    {
+      queNum: 6,
+      question: "Assertion – Dipole moment in case of BeF₂ is zero.\nReason – Two equal bond dipoles point in opposite directions and cancel effect of each other.",
+      options: [
+        "(1) Both Assertion and Reason are true and the Reason is a correct explanation of the Assertion.",
+        "(2) Both Assertion and Reason are true but Reason is not a correct explanation of the Assertion.",
+        "(3) Assertion is true but Reason is false.",
+        "(4) Both Assertion and Reason are false."
+      ],
+      answer: "(1) Both Assertion and Reason are true and the Reason is a correct explanation of the Assertion."
+    },
+    {
+      queNum: 7,
+      question: "Compare the dipole moment of following molecules –",
+      options: [
+        "(1) H₂O < H₂S < CO₂",
+        "(2) HF < HCl < H-Br",
+        "(3) NH₃ > NF₃ > BF₃",
+        "(4) CH₄ > CHCl₃ > CCl₄"
+      ],
+      answer: "(3) NH₃ > NF₃ > BF₃"
+    },
+    {
+      queNum: 8,
+      question: "Which of the following set represents positive overlap.",
+      options: [
+        "(1) [Figure option 1]",
+        "(2) [Figure option 2]",
+        "(3) [Figure option 3]",
+        "(4) All sets are correct"
+      ],
+      answer: "(3) [Figure option 3]",
+      img: "/assets/chemical-bonding-8.png"
+    },
+    {
+      queNum: 9,
+      question: "Which of the following relation represent a stable molecule in respect of MOT ?",
+      options: [
+        "(1) Nb > Na",
+        "(2) Na < Nb",
+        "(3) Nb = Na",
+        "(4) None of these"
+      ],
+      answer: "(1) Nb > Na"
+    },
+    {
+      queNum: 10,
+      question: "Find the incorrect valence electronic configuration for the following molecule–",
+      options: [
+        "(1) B₂: 2s 2s 2pσ σ σ",
+        "(2) O₂: 2s 2s 2p 2p 2p (σ σ σ π π)",
+        "(3) N₂: 2s 2s 2p 2p (σ σ π σ)",
+        "(4) C₂: 2s 2s 2p (σ σ π)"
+      ],
+      answer: "(1) B₂: 2s 2s 2pσ σ σ"
+    }
+  ];
+  
+  export default QUESTIONS;
+  
